ea2f2b400b15ba763fdbb2e8861c118608577223
Robson Silva <robsonads@outlook.com>
2019-01-04 17:19:55 -0300

    Contrato Inbox

---
+++ bank.sol
@@ -0,0 +1,41 @@
+pragma solidity 0.4.0;
+
+contract BankContract {
+
+    uint private balance;
+    address public owner;
+
+    event DepositEvent (uint amount, uint balance);
+    event WithdrawtEvent (uint amount, uint balance);
+
+    function BankContract(){
+        owner = msg.sender;
+    }
+
+    function deposit () public payable {
+        balance += msg.value;
+        DepositEvent(msg.value, balance);
+    }
+
+    function emprestimo(uint valor){
+        balance -= valor;
+        DepositEvent(msg.value, balance);
+    }
+
+    function withdraw (uint amount) public {
+        if(msg.sender == owner && balance >= amount){
+            balance -= amount;
+
+            if(!msg.sender.send(amount)){
+                balance += amount;
+            }else{
+                WithdrawtEvent(amount, balance);
+            }
+        }
+    }
+
+    function getBalance() public returns (uint){
+        return balance;
+    }
+
+}
